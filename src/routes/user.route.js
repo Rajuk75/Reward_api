7799c311375a45e2cd5ddef6f847661cbd50cb56
Ambika1210 <ambika.ambika1210@gmail.com>
2025-12-07 00:36:55 +0530

    admin api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,11 @@ const userController  = require("../controllers/user.controller");
 
 router.post("/v1/user/user-create", userController.createUserController);
 
+router.post("/v1/user/login", userController.loginUserController);
+router.post("/v1/user/admin-create", userController.createAdminController);
+router.get("/v1/user/all-user", userController.getAllUsersController);
+router.put("/v1/user/update/:id", userController.updateUserController);
+
 
 module.exports = router;
 
